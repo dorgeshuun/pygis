eeccc5c9adba9ad6cb2bbf06b270e34020ce1035
dorgeshuun <vincent.poulet@wanadoo.fr>
2024-01-27 12:59:13 +0100

    removed unnecessary imports

--- pygis/__main__.py
+++ pygis/__main__.py
@@ -7,11 +7,9 @@ from pygis.point import Point
 from pygis.feature import Feature, Attribute
 from pygis.map_widget import MapWidget
 
-import itertools
-from dataclasses import dataclass
-
 WIDTH = 800
 HEIGHT = 600
+
 
 def get_pts_from_file(file: TextIO):
     field_names = file.readline().strip().split(";")[2:]
@@ -19,7 +17,8 @@ def get_pts_from_file(file: TextIO):
     for line in file:
         fields = line.strip().split(";")
         point = Point.from_angular_coords(float(fields[0]), float(fields[1]))
-        attributes = [Attribute(field_names[i], x) for i, x in enumerate(fields[2:])]
+        attributes = [Attribute(field_names[i], x)
+                      for i, x in enumerate(fields[2:])]
         yield Feature(point, attributes)
 
 
